7b7d6a6e619a9d17ff2ccf5e4ebfb01223669ab0
mimim <mimimishany@gmail.com>
2025-05-29 00:04:22 +0300

    Створення представлення БД

---
+++ app/src/main/java/DAO.kt
@@ -0,0 +1,44 @@
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface DAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(users: Seeds)
+
+    @Query("SELECT * FROM seeds")
+    suspend fun getAll(): List<Seeds>
+
+
+}
+
+@Dao
+interface UsersDAAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(users: Users)
+
+    @Query("SELECT * FROM users")
+    suspend fun getAll(): List<Users>
+
+    @Query("SELECT * FROM users WHERE login = :login AND password = :password")
+    suspend fun doUserIsCreated(login: String, password: String): Users?
+
+
+
+}
+
+@Dao
+interface FuelDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(fuel_perationss: Fuel)
+
+    @Query("SELECT * FROM fuel")
+    suspend fun getAll(): List<Fuel>
+
+
+
+
+
+}
